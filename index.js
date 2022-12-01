6b1d0f69a27567fa618d3a3a93a14e0266118f72
saifRony <isaif326@gmail.com>
2022-12-02 01:59:44 +0600

    addad mongodb

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const app = express();
 const cors =require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// middleare
+app.use(cors());
+app.use(express.json());
+
+
+// name:laptop
+// password : zVq2XpeIr9hdNRur
+
+
+
+
+const uri = "mongodb+srv://laptop:zVq2XpeIr9hdNRur@cust1.baig3hx.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const laptopCollection =client.db('laptopCategory').collection('services');
+
+        // data
+
+        app.get('/sevices',async(req,res)=>{
+            const query ={};
+            const cursor =laptopCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log);
 
 app.get('/', (req, res) => {
     res.send('simple Node Server');
