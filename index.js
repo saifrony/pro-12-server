c36347a16b21be38858e8d57736d8c42d64d2569
saifRony <isaif326@gmail.com>
2022-12-02 21:19:29 +0600

    env

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cust1.baig3hx.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
